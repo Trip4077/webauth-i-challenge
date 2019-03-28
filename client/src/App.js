b8290583f9b47034df568fcd789a26f3b664d551
Trip4077 <bj2336@email.vccs.edu>
2019-03-27 21:28:17 -0400

    User can register, login, and then view user list

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,118 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {},
+      users: [],  
+      username: '',
+      password: ''
+    }
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  register = e => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    axios.post('http://localhost:4567/api/register', user)
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+  }
+
+  login = e => {
+    e.preventDefault();
+
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    axios.post('http://localhost:4567/api/login', user)
+    .then(res => {
+      console.log(res.data);
+
+      axios.defaults.headers.common['username'] = user.username;
+      axios.defaults.headers.common['password'] = user.password;
+
+      this.getUserList();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  getUserList = userInfo => {
+    axios.get('http://localhost:4567/api/users')
+        .then(res => {
+          console.log(res);
+          this.setState({ users: res.data })
+        })
+        .catch(err => {
+          console.log(err);
+        })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form>
+          <label>
+            Username
+          </label>
+
+          <input type='text'
+                 name='username'
+                 value={this.state.username}  
+                 onChange={this.handleChange}
+                 placeholder='username'
+                 />
+
+          <label>
+            Password
+          </label>
+          
+          <input type='text'
+                 name='password'
+                 value={this.state.password}  
+                 onChange={this.handleChange}
+                 placeholder='password'
+                 />
+
+          <button onClick={this.login}>
+            Login
+          </button>
+
+          <button onClick={this.register}>
+            Register
+          </button>
+        </form>
+
+        {!this.state.users.length ? undefined 
+                                  : this.state.users.map(user => <p key={Math.random()}>{user.username}</p>)}
       </div>
     );
   }
 }
+
 
 export default App;
